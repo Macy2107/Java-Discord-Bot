e76a401193e6c994e45131643600a3d44e3c2828
Hai My <macy.buihuynh@gmail.com>
2025-10-16 14:02:07 +0700

    git commit

--- src/main/java/org/example/DiscordBot.java
+++ src/main/java/org/example/DiscordBot.java
@@ -10,11 +10,18 @@ import javax.security.auth.login.LoginException;
 
 // SET UP BOT
 // RUNNING THE MAIN METHOD
-public class DiscordBot {
-    public static void main(String[] args) throws LoginException {
-        JDA kittenBot = JDABuilder.createDefault("MTQyNzEzMzk1ODczODU0Mjc1Mw.Gv-9_X.2ETsAOoSSQpdaSg-e-ETEf2CVkTovc2m92vN5k")
-                                  .setActivity(Activity.customStatus("Welcome to WiWiWi uwu"))
-                                  .build();
+public class DiscordBot extends ListenerAdapter {
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        // CONFIG THE TOKEN
+        properties.load(new FileInputStream("token.properties"));
+        // USING OOP AT PROPERTIES
+        String token = properties.getProperty("BOT_TOKEN");
+        JDA kittenBot = JDABuilder.createDefault(token)
+                .setActivity(Activity.customStatus("Welcome to WiWiWi uwu\n" +
+                        " ≽^• ˕ • ྀི≼"))
+                .addEventListeners(new DiscordBot())
+                .build();
 
         kittenBot.getPresence().setStatus(OnlineStatus.ONLINE);
         kittenBot.setAutoReconnect(true);
